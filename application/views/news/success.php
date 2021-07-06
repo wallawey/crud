5c8438fd220a57d94a07348e9142e6f14c9eb6e7
wallawey <mmakr1f@gmail.com>
2021-07-06 10:43:51 +0800

    Delete application/views/news directory

--- application/views/news/success.php
+++
@@ -1,2 +0,0 @@
- <link rel="stylesheet" type="text/css" href="<?php echo base_url('application/css/bootstrap-5.0.2-dist/css/bootstrap.css');?>" media="all"/>
-<p>News added successfully!</p>
--- application/views/news/view.php
+++
@@ -1,4 +0,0 @@
- <link rel="stylesheet" type="text/css" href="<?php echo base_url('application/css/bootstrap-5.0.2-dist/css/bootstrap.css');?>" media="all"/>
-<?php
-echo '<h2>'.$news_item['title'].'</h2>';
-echo $news_item['text'];
